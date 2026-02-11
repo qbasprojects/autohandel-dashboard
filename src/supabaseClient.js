515b780a9468af379673248de816bde1aa1d405a
Qbasprojects <jakubmazurekit@gmail.com>
2026-02-11 01:10:58 +0100

    Update supabase config for production

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://twascrvlzmcmvqhzyipv.supabase.co'; // np. https://xxxxx.supabase.co
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3YXNjcnZsem1jbXZxaHp5aXB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NTk4MjUsImV4cCI6MjA4NjMzNTgyNX0.FXrmwzU4y8tfj4xnpA4LX0kQTYPmI6Xgh3I12eYEM-g'; // długi klucz z Supabase
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'TWÓJ_FALLBACK_URL';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'TWÓJ_FALLBACK_KEY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
